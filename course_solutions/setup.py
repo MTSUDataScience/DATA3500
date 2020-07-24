66e6e42c4b423794a63f8f421668f1e0980973cf
MTSUDataScience <67022422+MTSUDataScience@users.noreply.github.com>
2020-07-24 13:37:18 -0500

    Update setup.py

--- course_solutions/setup.py
+++ course_solutions/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='data3500',
       version='0.0.2',
       description='Module for Data 3500 MTSU Course',
-      url='https://github.com/MTSUDataScience/DATA3500/course_solutions',
+      url='https://github.com/MTSUDataScience/DATA3500',
       author='Jeff Stark',
       author_email='jeff.stark@mtsu.edu',
       packages=['solutions'])
