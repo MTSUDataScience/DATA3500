83634841ba4efc073708bbd5db7575e44099b401
MTSUDataScience <67022422+MTSUDataScience@users.noreply.github.com>
2020-07-24 13:38:19 -0500

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='data3500',
+      version='0.0.2',
+      description='Module for Data 3500 MTSU Course',
+      url='https://github.com/MTSUDataScience/DATA3500',
+      author='Jeff Stark',
+      author_email='jeff.stark@mtsu.edu',
+      packages=['solutions'])
