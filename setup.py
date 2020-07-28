1247a574d5ec6591d731cee2c369bfea08eab9e7
Sara Shirley <sarashirley@a04998sshirley.attlocal.net>
2020-07-28 08:20:26 -0500

    testing v3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='data3500',
-      version='0.0.2',
+      version='0.0.3',
       description='Module for Data 3500 MTSU Course',
       url='https://github.com/MTSUDataScience/DATA3500',
       author='Jeff Stark',
